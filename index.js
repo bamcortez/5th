1d5cd2676ff27bd0322e49ac6428684d73844f77
bamcortezz <2022312593@dhvsu.edu.ph>
2024-07-16 09:05:53 +0800

    5th

---
+++ index.js
@@ -0,0 +1,4 @@
+const envelopeWrapper = document.querySelector('.envelope-wrapper');
+      envelopeWrapper.addEventListener('click', () => {
+         envelopeWrapper.classList.toggle('flap');
+      });
